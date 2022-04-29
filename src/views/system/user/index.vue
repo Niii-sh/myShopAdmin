1310f7aff9d0a13baec1e3f300f8e3a10680782b
Niii <1159417019@qq.com>
2022-04-29 11:43:35 +0800

    完成了用户管理界面的基本样式编写

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -1,40 +1,140 @@
 <template>
-  <el-table
-      :data="tableData"
-      style="width: 100%">
-    <el-table-column
-        label="日期"
-        width="180">
-      <template slot-scope="scope">
-        <i class="el-icon-time"></i>
-        <span style="margin-left: 10px">{{ scope.row.date }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column
-        label="姓名"
-        width="180">
-      <template slot-scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>姓名: {{ scope.row.name }}</p>
-          <p>住址: {{ scope.row.address }}</p>
-          <div slot="reference" class="name-wrapper">
-            <el-tag size="medium">{{ scope.row.name }}</el-tag>
-          </div>
-        </el-popover>
-      </template>
-    </el-table-column>
-    <el-table-column label="操作">
-      <template slot-scope="scope">
-        <el-button
-            size="mini"
-            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-        <el-button
-            size="mini"
-            type="danger"
-            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <div class="head-container">
+      <!--增删改查按钮-->
+      <div class="crud-opts">
+        <span class="crud-opts-left">
+          <!--左侧插槽-->
+          <slot name="left"/>
+          <el-button
+              class="filter-item"
+              size="mini"
+              type="primary"
+              icon="el-icon-plus"
+          >
+            新增
+          </el-button>
+          <el-button
+              class="filter-item"
+              size="mini"
+              type="success"
+              icon="el-icon-edit"
+          >
+            修改
+          </el-button>
+          <el-button
+              slot="reference"
+              class="filter-item"
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+          >
+            删除
+          </el-button>
+          <el-button
+              class="filter-item"
+              size="mini"
+              type="warning"
+              icon="el-icon-download"
+          >
+            导出
+          </el-button>
+          <!--右侧-->
+          <slot name="right"/>
+        </span>
+        <el-button-group class="crud-opts-right">
+          <el-button
+              size="mini"
+              plain
+              type="info"
+              icon="el-icon-search"
+          />
+          <el-button
+              size="mini"
+              icon="el-icon-refresh"
+          />
+          <el-popover
+              placement="bottom-end"
+              width="150"
+              trigger="click"
+          >
+            <el-button
+                slot="reference"
+                size="mini"
+                icon="el-icon-s-grid"
+            >
+              <i
+                  class="fa fa-caret-down"
+                  aria-hidden="true"
+              />
+            </el-button>
+            <el-checkbox>
+              全选
+            </el-checkbox>
+          </el-popover>
+        </el-button-group>
+      </div>
+    </div>
+    <!--用户列表信息表格-->
+    <el-table
+        :data="tableData"
+        style="width: 100%"
+    >
+      <el-table-column
+          fixed prop="username"
+          label="用户名"
+          width="150">
+      </el-table-column>
+      <el-table-column
+          prop="nickName"
+          label="昵称"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          prop="gender"
+          label="性别"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          prop="phone"
+          label="电话"
+          width="200">
+      </el-table-column>
+      <el-table-column
+          prop="email"
+          label="邮箱"
+          width="200">
+      </el-table-column>
+      <el-table-column
+          prop="dept.name"
+          label="部门"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          prop="enabled"
+          label="状态"
+          width="120">
+        <template slot-scope="scope">
+          <el-switch ref="enabled"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column
+          :show-overflow-tooltip="true"
+          prop="createTime"
+          label="创建日期"
+          width="200">
+      </el-table-column>
+      <el-table-column
+          prop="operation"
+          label="操作"
+          width="120">
+        <template slot-scope="scope">
+          <el-button type="text" size="small">Edit</el-button>
+          <el-button type="text" size="small">Delete</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
